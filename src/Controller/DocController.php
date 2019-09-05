91ea2bed9d0ccf30a8b5fce85cc4de18bb6d4962
Thor Skjelbreia <thor.skjelbreia@evry.com>
2019-09-05 11:30:39 +0200

    Use ghmarkdown for markdown to html parse needs

--- src/Controller/DocController.php
+++ src/Controller/DocController.php
@@ -9,84 +9,67 @@ namespace Drupal\doc_page\Controller;
 use Drupal\Component\Utility\UrlHelper;
 use Drupal\Core\Link;
 use Drupal\Component\Utility\Html;
-use Michelf\MarkdownExtra;
+use Drupal\ghmarkdown\cebe\markdown\MarkdownExtra;
 
 class DocController {
-    public function content() {
+  public function content() {
 
-        $error = array(
-            '#markup' => 'No documentation found. Please contact an administrator.',
-        );
+    $error = array(
+      '#markup' => 'No documentation found. Please contact an administrator.',
+    );
 
-        // Get the current user
-        $user = \Drupal::currentUser();
+    // Get the current user
+    $user = \Drupal::currentUser();
 
-        // Check for permission
-        if ($user->hasPermission('administer doc page')) {
-        $admin_link = Link::createFromRoute('Administer documentation page', 'doc.settings', []);
-            $error['#markup'] = 'No URL is specified or the URL is bad.<br><br>' . $admin_link->toString();
-        }
-
-        //Get values
-        $config = \Drupal::config('doc.adminsettings');
-        $url = $config->get('doc_url');
-        $markdown = $config->get('doc_markdown');
-        $valid = UrlHelper::isValid($url, $absolute = TRUE);
-        if (!isset($url) || !isset($markdown)) {
-            return $error;
-        }
-
-        if (!isset($markdown)) {
-
-            // Get content.
-            $path = $url;
-            $html = file_get_contents($path);
-            if (!isset($html) || $html == '') {
-                return $error;
-            }
-
-            // Remove script tag messing stuff up.
-            $html = preg_replace('#<script(.*?)>(.*?)</script>#is', '', $html);
-            // Remove code{} part at the begging of the document.
-            $html = preg_replace('#code{(.*?)}#is', '', $html);
-
-            $doc = Html::load($html);
-            $imageTags = $doc->getElementsByTagName('img');
-            // Get images from source site.
-            foreach($imageTags as $tag) {
-                $src = $tag->getAttribute('src');
-                $tag->setAttribute('src', $path.$src);
-            }
-
-            // Return html.
-            return array(
-                '#type' => 'markup',
-                '#markup' =>  '<div class="documentation-wrap">' . Html::serialize($doc) . '</div>',
-            );
-        }
-
-        if (isset($markdown[0])) {
-            #include_once \Drupal::root() . '/modules/custom/doc_page/parsedown/Parsedown.php';
-            #include_once drupal_get_path('module', 'doc_page');
-            $markdown_display = '';
-            $file = \Drupal::entityTypeManager()->getStorage('file')->load($markdown[0]);
-            $text = file_get_contents($file->url());
-
-           $markdown_display = MarkdownExtra::defaultTransform($text);
-            #$markdown_display = Parsedown::instance()->text('Hello _Parsedown_!');
-            #dpm($parsedown);
-
-            #dpm($markdown_display);
-                        // Return html.
-                        return array(
-                            '#type' => 'markup',
-                            '#markup' =>  '<div class="documentation-wrap">' . $markdown_display . '</div>',
-                        );
-        }
-        // Return html.
-        return array(
-            '#type' => 'markup',
-            '#markup' =>  '<div class="documentation-wrap">Oh boy</div>',
-        );
+    // Check for permission
+    if ($user->hasPermission('administer doc page')) {
+      $admin_link = Link::createFromRoute('Administer documentation page', 'doc.settings', []);
+      $error['#markup'] = 'No URL is specified or the URL is bad.<br><br>' . $admin_link->toString();
     }
+
+    //Get values
+    $config = \Drupal::config('doc.adminsettings');
+    $url = $config->get('doc_url');
+    $markdown = $config->get('doc_markdown');
+
+    // If no values are set return a default markup.
+    if (!isset($url) || !isset($markdown)) {
+      return $error;
+    }
+
+    if (!isset($markdown) || $markdown == '' && isset($url) && $url != '') {
+
+      // Get content.
+      $path = $url;
+      $html = file_get_contents($path);
+      if (!isset($html) || $html == '') {
+        return $error;
+      }
+
+      // Remove code{} part at the begging of the document.
+      $html = preg_replace('#code{(.*?)}#is', '', $html);
+
+      $doc = Html::load($html);
+      $imageTags = $doc->getElementsByTagName('img');
+      // Get images from source site.
+      foreach($imageTags as $tag) {
+        $src = $tag->getAttribute('src');
+        $tag->setAttribute('src', $path.$src);
+      }
+      $text = Html::serialize($doc);
+    }
+
+    if (isset($markdown[0])) {
+      $file = \Drupal::entityTypeManager()->getStorage('file')->load($markdown[0]);
+      $text = file_get_contents($file->url());
+    }
+
+    $markdown = new MarkdownExtra();
+    $markdown_display = $markdown->parse($text);
+    // Return html.
+    return array(
+    '#type' => 'markup',
+    '#markup' =>  '<div class="documentation-wrap">' . $markdown_display . '</div>',
+    );
+  }
 }
--- src/Form/DocSettingsForm.php
+++ src/Form/DocSettingsForm.php
@@ -13,84 +13,92 @@ use Drupal\Component\Utility\UrlHelper;
  */
 class DocSettingsForm extends ConfigFormBase {
 
-    /**
-    * {@inheritdoc}
-    */
-    protected function getEditableConfigNames() {
-        return [
-            'doc.adminsettings',
-        ];
+  /**
+  * {@inheritdoc}
+  */
+  protected function getEditableConfigNames() {
+    return [
+      'doc.adminsettings',
+    ];
+  }
+
+  /**
+  * {@inheritdoc}
+  */
+  public function getFormId() {
+    return 'doc_page_settings';
+  }
+
+  /**
+  * {@inheritdoc}
+  */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('doc.adminsettings');
+
+    $form['doc_url'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Url'),
+      '#description' => $this->t('Enter documentation url'),
+      '#default_value' => $config->get('doc_url'),
+    ];
+
+    $validators = array(
+      'file_validate_extensions' => array('md'),
+    );
+
+    $form['doc_markdown'] = array(
+      '#type' => 'managed_file',
+      '#name' => 'doc_markdown',
+      '#title' => t('File *'),
+      '#size' => 20,
+      '#description' => t('Markdown format only'),
+      '#default_value' => $config->get('doc_markdown'),
+      '#upload_validators' => $validators,
+      '#upload_location' => 'public://doc_markdowns/',
+    );
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+  * {@inheritdoc}
+  */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    parent::submitForm($form, $form_state);
+    // Save file.
+    if (isset($form_state->getValue('doc_markdown')[0])) {
+      $file_value = $form_state->getValue('doc_markdown');
+      $file = \Drupal::entityTypeManager()->getStorage('file')->load($file_value[0]);
+      $file->setPermanent();
+      $file->save();
+
+      $this->config('doc.adminsettings')
+        ->set('doc_markdown', $file_value)
+        ->save();
+    } else {
+      // Remove file.
+      $this->config('doc.adminsettings')
+        ->set('doc_markdown', FALSE)
+        ->save();
     }
+    // Save url.
+    $this->config('doc.adminsettings')
+      ->set('doc_url', $form_state->getValue('doc_url'))
+      ->save();
+  }
 
-    /**
-    * {@inheritdoc}
-    */
-    public function getFormId() {
-        return 'doc_page_settings';
+  /**
+  * {@inheritdoc}
+  */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    // Make sure one of the fields has a value.
+    if ($form_state->getValue('doc_markdown') == NULL && $form_state->getValue('doc_url') == NULL) {
+      $form_state->setErrorByName('doc_markdown', $this->t('Either file or url is required.'));
+      $form_state->setErrorByName('doc_url', $this->t("Either file or url is required."));
     }
-
-    /**
-    * {@inheritdoc}
-    */
-    public function buildForm(array $form, FormStateInterface $form_state) {
-        $config = $this->config('doc.adminsettings');
-
-        $form['doc_url'] = [
-            '#type' => 'textfield',
-            '#title' => $this->t('Url'),
-            '#description' => $this->t('Enter documentation url'),
-            '#default_value' => $config->get('doc_url'),
-        ];
-
-        $validators = array(
-            'file_validate_extensions' => array('md'),
-        );
-        $form['doc_markdown'] = array(
-            '#type' => 'managed_file',
-            '#name' => 'doc_markdown',
-            '#title' => t('File *'),
-            '#size' => 20,
-            '#description' => t('Markdown format only'),
-            '#default_value' => $config->get('doc_markdown'),
-            '#upload_validators' => $validators,
-            '#upload_location' => 'public://doc_markdowns/',
-        );
-
-        return parent::buildForm($form, $form_state);
+    // And check the url if set.
+    if ($form_state->getValue('doc_url') != '' &&   UrlHelper::isValid($form_state->getValue('doc_url'), TRUE) == FALSE) {
+      $form_state->setErrorByName('doc_url', $this->t("This URL doesn't look right"));
     }
-
-    /**
-    * {@inheritdoc}
-    */
-    public function submitForm(array &$form, FormStateInterface $form_state) {
-        parent::submitForm($form, $form_state);
-        // Save file.
-        if (isset($form_state->getValue('doc_markdown')[0])) {
-            $file_value = $form_state->getValue('doc_markdown');
-            $file = \Drupal::entityTypeManager()->getStorage('file')->load($file_value[0]);
-            $file->setPermanent();
-            $file->save();
-
-            $this->config('doc.adminsettings')
-            ->set('doc_markdown', $file_value)
-            ->save();
-        }
-
-        $this->config('doc.adminsettings')
-          ->set('doc_url', $form_state->getValue('doc_url'))
-          ->save();
-    }
-
-    /**
-    * {@inheritdoc}
-    */
-    public function validateForm(array &$form, FormStateInterface $form_state) {
-        if ($form_state->getValue('doc_url') != '' &&   UrlHelper::isValid($form_state->getValue('doc_url'), TRUE) == FALSE) {
-            $form_state->setErrorByName('doc_url', $this->t("This URL doesn't look right"));
-        }
-        if ($form_state->getValue('doc_markdown') == NULL && $form_state->getValue('doc_url') == NULL) {
-            $form_state->setErrorByName('doc_markdown', $this->t('Either file or url is required.'));
-            $form_state->setErrorByName('doc_url', $this->t("Either file or url is required."));
-        }
-    }
+  }
 }
